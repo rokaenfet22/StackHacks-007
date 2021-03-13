ac65bbf3dc3497417728d9cad43b36f2cbfbc228
Fusuke Minagi <fussinboots22@gmail.com>
2021-03-14 00:36:29 +0900

    reset dir for package.json, removed ron's client.js

--- app/client/index.js
+++ app/client/index.js
@@ -1,9 +1,4 @@
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
       mapTypeControl: false,
@@ -79,9 +74,6 @@ function initMap() {
           this.destinationPlaceId = place.place_id;
         }
 
-        console.log(this.originPlaceId)
-        console.log(this.destinationPlaceId)
-
         this.route();
       });
     }
@@ -108,3 +100,30 @@ function initMap() {
 }
 
 
+
+
+
+/*cameron's client.js
+async function retrieveDetails () {
+    const start = 
+    const destination = 
+    const paras = { start: start, destination: destination };
+    const response = await fetch('http://127.0.0.1:8090/getDetails', {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(paras)
+    });
+    const details = await response.json();
+    for (const emissions in details) {
+        
+    }
+    filler(distance);
+}
+
+function filler (distance) {
+    modesOfTravel = ['car', 'train', 'plane', 'helicopter', 'submarine', 'carPlane', 'blimp', 'hotAirBalloon'];
+    for (const mode in modesOfTravel) {
+        document.getElementById(mode).innerHTML = carbonEmission(mode, distance);
+    }
+}
+*/
