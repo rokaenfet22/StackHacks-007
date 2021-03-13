033a3d5fe5ff9fe8462fa2c2883c43952b0c0883
Fusuke Minagi <fussinboots22@gmail.com>
2021-03-14 00:15:53 +0900

    server side created

---
+++ app/client/index.js
@@ -0,0 +1,110 @@
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      mapTypeControl: false,
+      center: { lat: -33.8688, lng: 151.2195 },
+      zoom: 13,
+    });
+    new AutocompleteDirectionsHandler(map);
+  }
+  
+  class AutocompleteDirectionsHandler {
+    constructor(map) {
+      this.map = map;
+      this.originPlaceId = "ChIJFRrlxmGHfkgRy8-a4viXKO0"; //Default Durham Cathedral
+      this.destinationPlaceId = "ChIJ6W3FzTRydkgRZ0H2Q1VT548"; //Default Heathrow Airport
+      this.travelMode = google.maps.TravelMode.WALKING;
+      this.directionsService = new google.maps.DirectionsService();
+      this.directionsRenderer = new google.maps.DirectionsRenderer();
+      this.directionsRenderer.setMap(map);
+      const originInput = document.getElementById("origin-input");
+      const destinationInput = document.getElementById("destination-input");
+      const modeSelector = document.getElementById("mode-selector");
+      const originAutocomplete = new google.maps.places.Autocomplete(originInput);
+      // Specify just the place data fields that you need.
+      originAutocomplete.setFields(["place_id"]);
+      const destinationAutocomplete = new google.maps.places.Autocomplete(
+        destinationInput
+      );
+      // Specify just the place data fields that you need.
+      destinationAutocomplete.setFields(["place_id"]);
+      this.setupClickListener(
+        "changemode-walking",
+        google.maps.TravelMode.WALKING
+      );
+      /*this.setupClickListener(
+        "changemode-transit",
+        google.maps.TravelMode.TRANSIT
+      );
+      */
+      this.setupClickListener(
+        "changemode-driving",
+        google.maps.TravelMode.DRIVING
+      );
+      this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+      this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+        destinationInput
+      );
+      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+    }
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    setupClickListener(id, mode) {
+      const radioButton = document.getElementById(id);
+      radioButton.addEventListener("click", () => {
+        this.travelMode = mode;
+        this.route();
+      });
+    }
+    setupPlaceChangedListener(autocomplete, mode) {
+      autocomplete.bindTo("bounds", this.map);
+      autocomplete.addListener("place_changed", () => {
+        const place = autocomplete.getPlace();
+  
+        if (!place.place_id) {
+          window.alert("Please select an option from the dropdown list.");
+          return;
+        }
+  
+        if (mode === "ORIG") {
+          this.originPlaceId = place.place_id;
+        } else {
+          this.destinationPlaceId = place.place_id;
+        }
+
+        console.log(this.originPlaceId)
+        console.log(this.destinationPlaceId)
+
+        this.route();
+      });
+    }
+    route() {
+      if (!this.originPlaceId || !this.destinationPlaceId) {
+        return;
+      }
+      const me = this;
+      this.directionsService.route(
+        {
+          origin: { placeId: this.originPlaceId },
+          destination: { placeId: this.destinationPlaceId },
+          travelMode: this.travelMode,
+        },
+        (response, status) => {
+          if (status === "OK") {
+            me.directionsRenderer.setDirections(response);
+          } else {
+            window.alert("Directions request failed due to " + status);
+          }
+        }
+      );
+    }
+}
+
+
---
+++ app/server.js
@@ -0,0 +1,11 @@
+//Extract Modules
+const express = require("express")
+const body_parser = require("body-parser")
+
+//Immediate use of modules
+const app = express()
+app.use(express.static("./client")) //serving static files from /client dir
+app.use(express.json()) //Parsing json-encoded bodies
+
+app.listen("8080")
+console.log("Server running at http://127.0.0.1:8080/")
